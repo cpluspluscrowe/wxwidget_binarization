5178b1eb895bfc80c9effecb196dc248d5414bc8
Chad Crowe <ccrowe@linkedin.com>
2019-10-10 20:52:17 -0500

    added functionality for image loading

--- imageBinarization.py
+++ imageBinarization.py
@@ -23,6 +23,7 @@ cv2.imwrite('gray_image.png',gray_image)
 
 # load the gray image
 gray_image = cv2.imread("./gray_image.png")
+cv2.imwrite("current_image.png", gray_image)
 
 def myfunc(a, b):
     "Return a-b if a>b, otherwise return a+b"
@@ -39,13 +40,11 @@ vfunc = np.vectorize(myfunc)
 
 import wx
 
-def onButton(event):
-    print
-    "Button pressed."
+
 
 class Mywin(wx.Frame):
     def __init__(self, parent, title):
-        super(Mywin, self).__init__(parent, title=title, size=(500, 500))
+        super(Mywin, self).__init__(parent, title=title, size=(500, 600))
         self.InitUI()
 
     def InitUI(self):
@@ -58,31 +57,51 @@ class Mywin(wx.Frame):
 
         vbox.Add(self.sld, 1, flag=wx.EXPAND | wx.ALIGN_CENTER_HORIZONTAL | wx.TOP, border=20)
         self.sld.Bind(wx.EVT_SLIDER, self.OnSliderScroll)
+
         self.txt = wx.StaticText(self.pnl, label='', style=wx.ALIGN_CENTER)
         vbox.Add(self.txt, 1, wx.ALIGN_CENTRE_HORIZONTAL)
-        self.refresh_image()
+
+        self.fb = wx.Button(self.pnl, wx.ID_ANY, 'Choose Image', (10, 100))
+
+        self.fb.Bind(wx.EVT_BUTTON, self.onButton)
+        self.fb.SetPosition((100, 500))
+        self.fb.SetBackgroundColour("Dark Grey")
+
+        self.path = "current_image.png"
+        self.gray_image = gray_image
+        self.refresh_image(gray_image)
         self.pnl.SetSizer(vbox)
         self.Centre()
         self.Show(True)
-    def refresh_image(self):
 
-        bmp = wx.Bitmap("./gray_image.png", wx.BITMAP_TYPE_ANY)
+    def onButton(self, event):
+        openFileDialog = wx.FileDialog(self.pnl, "Open", "", "",
+                                       "Python files (*.png)|*.png",
+                                       wx.FD_OPEN | wx.FD_FILE_MUST_EXIST)
 
+        openFileDialog.ShowModal()
+        openFileDialog.Destroy()
+        self.path = openFileDialog.GetPath()
+        self.gray_image = cv2.imread(self.path)
+        self.refresh_image(self.gray_image)
+        print(self.path)
+
+    def refresh_image(self, image):
+        cv2.imwrite("temp.png", image)
+        bmp = wx.Bitmap("temp.png", wx.BITMAP_TYPE_ANY)
         button = wx.BitmapButton(self.pnl, id=wx.ID_ANY, bitmap=bmp,
                                  size=(300,300))
-        button.Bind(wx.EVT_BUTTON, onButton)
+        button.Bind(wx.EVT_BUTTON, lambda x: x)
         button.SetPosition((100, 175))
+        cv2.imwrite("current_image.png", image)
 
     def OnSliderScroll(self, e):
         obj = e.GetEventObject()
         val = obj.GetValue()
-        thresh = vfunc(gray_image, val)
+        thresh = vfunc(self.gray_image, val)
         #ret, thresh = cv2.threshold(gray_image, val, 255, cv2.THRESH_BINARY)
-        cv2.imwrite('gray_image.png', thresh)
-        self.refresh_image()
-        #font = self.GetFont()
-        #font.SetPointSize(self.sld.GetValue())
-        #self.txt.SetFont(font)
+        self.refresh_image(thresh)
+
 
 
 ex = wx.App()
